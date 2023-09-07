75933ab95495a54af2bc3a2565b2c29944d3ccd7
Unknownmaster0 <sagar192002singh@gmail.com>
2023-09-07 22:16:38 +0530

    this is the python

---
+++ input_in_python.py
@@ -0,0 +1,11 @@
+# input in python -> it always takes the argument in string format.
+#   we need to typecast the string into integer.
+
+x = input("enter numbers: ")
+y = input("enter number: ")
+# here x and y is in string. so, typecast it to integer.
+print("sum of x + y =",int(x) + int(y))
+
+
+
+
--- stringINPython.py
+++ stringINPython.py
@@ -8,6 +8,15 @@ print(name[0:3])  # this will print the 0 to (3-1) characters of the name
 print(name[4:7])  # this will print the all the characters that in its range. And doesn't give any error.
 print(name[5:7])  # this will give no error and simply doesn't print anything, when we write outside the boundry of string.
 
+# kitne tareke se string ko likh sakte hai python me.
+print("different ways to initialise the string in python.")
+name1 = 'sagar'
+name2 = "sagar"
+name3 = '''sagar'''
+
+print(name1, name2, name3)
+
+
 # methods in strings in python
 print(name.count("a"))  # count the occurrence of given character in string.
 print(name.capitalize())  # capitalise the first character of the string.
