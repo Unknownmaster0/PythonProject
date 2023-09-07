330da6c1c471878cecdd61f947faca4a2af426a3
Unknownmaster0 <sagar192002singh@gmail.com>
2023-09-07 18:21:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# variable in python
+a = 3
+print(a)
+
+# printing the variables in python
+a = 6.2
+print(a)
+
+# Note that the same variable name can store different type of data types.
+a = "sagar"
+print(a)
+
+a = None  # a new data type in python
+print(a)
+
+# Type-Casting in python
+s = "5"
+print(s)
+# here s and t are holding the same value but there data type is different. Here, s is string and t is integer.
+t = 5
+print(t)
+
+# difference come in both when we add any number in both
+# print(s+1)  # this line will throw error, so we use typecasting technique.
+print(int(s) + 1)  # here we type-casted the string to integer
+print(t+1)
---
+++ operators.py
@@ -0,0 +1,51 @@
+num1 = 5
+num2 = 9
+
+# Arithmetic operators
+print("num1 + num2",num1+num2)
+print("num1 - num2",num1-num2)
+print("num1 * num2",num1*num2)
+print("num1 / num2",num1/num2)
+
+# floor operator -> //
+print(10//3)  # it will give me floor value only. without decimal value.
+
+# exponential operator -> ** calculate the power
+print(10 ** 3)
+
+# Assignment operators in python is same as other languages.
+"""
+1. += 
+2. -=
+3. *=
+4. /=
+5. //=
+6. **=
+7. %=
+
+"""
+
+
+# comparison operators
+"""
+Note -> the answer of comparison operators is always in BOOLEAN.
+1. >
+2. <
+3. >=
+4. <=
+5. !=
+"""
+
+# Logical operators
+"""
+Note-> the answer of logical operators are also in true or false i.e. in boolean only.
+1. and
+2. or
+
+"""
+
+a = 5
+b = 6
+c = 5
+print(a > b and a == c)
+print(a > b or a == c)
