82f758039981dcf9e48e9d9c8fcfffe12e59c724
Unknownmaster0 <sagar192002singh@gmail.com>
2023-09-08 00:19:29 +0530

    match case and one pythom program of table.

---
+++ match_cases.py
@@ -0,0 +1,20 @@
+# match case == switch cases in C++/C
+
+a = int(input('enter number b/w 1 to 5: '))
+
+match a:
+    case 1:
+        print("I am 1.")
+    case 2:
+        print("I am 2.")
+    case 3:
+        print("I am 3.")
+    case 4:
+        print("I am 4.")
+    case 5:
+        print("I am 5.")
+    case _:
+        print("No, matching cases.This is default case.")
+
+
+
